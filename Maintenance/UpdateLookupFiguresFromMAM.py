e2a3dcf272cbd8a5741e50462a82ce5e1eb51c0d
tonywaddle <tonywaddle@gmx.com>
2021-02-12 17:14:07 +0000

    Added logic to add BatchManagerUser to db_owner for restored VRM database.

--- Maintenance/UpdateLookupFiguresFromMAM.py
+++ Maintenance/UpdateLookupFiguresFromMAM.py
@@ -17,7 +17,11 @@ processFile = cf.get_bool(f'Latest file is: {cf.bcolors.OKGREEN}{latest_file}.{c
 query = csf.restoreDatabase('vrm', latest_file, 'VRM_Log', 'VRM_Data')
 
 if cf.get_bool('Please run the following SQL statement on 192.168.0.32:'
-            f'\n{cf.bcolors.OKBLUE}{query}{cf.bcolors.ENDC}'
+            f'\n{cf.bcolors.OKBLUE}{query}'
+               f'\nUSE vrm'
+               f'\nCREATE USER BatchManagerUser FOR LOGIN BatchManagerUser'
+               f"\nEXEC sys.sp_addrolemember @rolename = 'db_owner', @membername = 'BatchManagerUser'"
+               f'{cf.bcolors.ENDC}'
             f'\nEnter true to continue once the backup is complete.'):
     csf.executeSQL('EXEC MAMData.dbo.UpdateCountsFromVRM')
 
